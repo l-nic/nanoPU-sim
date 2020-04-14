1ea6aaa16863f92a90d0e77d200e401b9a64b84f
Serhat Arslan <sarslan@stanford.edu>
2020-04-14 10:04:21 -0400

    Homa header is defined

--- headers.py
+++ headers.py
@@ -7,7 +7,8 @@ NDP_PROTO = 0x99
 class NDP(Packet):
     name = "NDP"
     fields_desc = [
-        FlagsField("flags", 0, 8, ["DATA", "ACK", "NACK", "PULL", "CHOP", "F1", "F2", "F3"]),
+        FlagsField("flags", 0, 8, ["DATA", "ACK", "NACK", "PULL",
+                                   "CHOP", "F1", "F2", "F3"]),
         ShortField("src_context", 0),
         ShortField("dst_context", 0),
         ShortField("tx_msg_id", 0),
@@ -15,6 +16,21 @@ class NDP(Packet):
         ShortField("pkt_offset", 0) # or should this be byte offset? Or should the header include both?
     ]
 
+class HOMA(Packet):
+    name = "HOMA"
+    fields_desc = [
+        FlagsField("op_code", 0, 8, ["ALL_DATA", "DATA", "GRANT",
+                                     "LOG_TIME_TRACE", "RESEND",
+                                     "BUSY", "ABORT", "BOGUS"]),
+        ShortField("rpc_id", 0), # should be unique for every RPC
+        FlagsField("flags", 0, 4, ["FROM_SERVER", "FROM_CLIENT",
+                                   "RETRANSMISSION", "RESTART"]),
+        ShortField("msg_len", 0),
+        ShortField("pkt_offset", 0), # or should this be byte offset? Or should the header include both?
+        ShortField("unscheduled_pkts", 0), # or should this be in bytes? Or should the header include both?
+        ShortField("prio",8),
+        ShortField("tx_msg_id", 0) # originally not in Homa definition, but required for NanoTransport architecture
+    ]
 
 class App(Packet):
     name = "App"
@@ -32,4 +48,3 @@ class SimMessage(Packet):
 
 bind_layers(IP, NDP, proto=NDP_PROTO)
 bind_layers(App, SimMessage)
-
